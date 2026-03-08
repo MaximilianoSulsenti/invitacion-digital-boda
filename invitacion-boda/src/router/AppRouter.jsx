2d88d6b24ee2de623c516f980a2dd92560c9dd75
maximilianosulsenti <maximilianosulsenti@gmail.com>
2026-03-07 21:21:34 -0300

    configuracion

--- invitacion-boda/src/router/AppRouter.jsx
+++ invitacion-boda/src/router/AppRouter.jsx
@@ -14,32 +14,32 @@ import AdminLayout from "../components/layout/AdminLayout";
 const AppRouter = () => {
   return (
     <Routes>
+  {/* 1. Rutas fijas (Prioridad Alta) */}
+  <Route path="/" element={<Home />} />
+  <Route path="/salon" element={<Salon />} />
+  <Route path="/fotos" element={<SalonFotos />} />
+  <Route path="/admin/login" element={<LoginAdmin />} />
 
-      {/* 🌍 Público */}
-      <Route path="/" element={<Home />} />
-      <Route path="/:linkUnico" element={<Invitacion />} />
-      <Route path="/galeria/:linkUnico" element={<Galeria />} />
-      <Route path="/salon" element={<Salon />} />
-      <Route path="/fotos" element={<SalonFotos />} />
+  {/* 2. Rutas con parámetros específicos */}
+  <Route path="/galeria/:linkUnico" element={<Galeria />} />
 
-      {/* 🔐 Login */}
-      <Route path="/admin/login" element={<LoginAdmin />} />
+  {/* 3. El comodín de invitación (DEBE IR AL FINAL de las públicas) */}
+  <Route path="/:linkUnico" element={<Invitacion />} />
 
-      {/* 🔐 Panel Admin */}
-      <Route
-        path="/admin"
-        element={
-          <ProtectedRoute>
-            <AdminLayout />
-          </ProtectedRoute>
-        }
-      >
-        <Route path="panel" element={<AdminDashboard />} />
-        <Route path="salon" element={<AdminSalon />} />
-      </Route>
-
-    </Routes>
+  {/* 🔐 Panel Admin */}
+  <Route
+    path="/admin"
+    element={
+      <ProtectedRoute>
+        <AdminLayout />
+      </ProtectedRoute>
+    }
+  >
+    <Route path="panel" element={<AdminDashboard />} />
+    <Route path="salon" element={<AdminSalon />} />
+  </Route>
+</Routes>
   );
-};
+}
 
 export default AppRouter;
